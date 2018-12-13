9a9cd9b45e9ceface709f364fe01669da42a084d
alex9702 <alexsandromoreir@gmail.com>
2018-12-13 12:24:41 -0300

    wallet

--- Blockchain/Wallet/wallet.py
+++ Blockchain/Wallet/wallet.py
@@ -2,5 +2,6 @@ from Crypto.PublicKey import RSA
 import binascii
 import Crypto.Random as rnd
 
-pr = RSA.generate(1024, rnd.new().read)
-print((binascii.hexlify(pr.exportKey()).decode('ascii')))
+
+pr = RSA.generate(1024,  rnd.new().read)
+pk = pr.publickey()
