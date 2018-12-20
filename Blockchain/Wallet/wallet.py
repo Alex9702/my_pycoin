7700eba31f26a1d0def9969e5910fcc0abf3a5bb
alex9702 <alexsandromoreir@gmail.com>
2018-12-20 16:39:33 -0300

    commit 20122018

--- Blockchain/Wallet/wallet.py
+++ Blockchain/Wallet/wallet.py
@@ -2,18 +2,19 @@ from Crypto.PublicKey import RSA
 import binascii
 import Crypto.Random
 
-# public_key = binascii.hexlify(RSA.generate(1024, Crypto.Random.new().read).exportKey(format='DER')).decode('ascii')
+public_key = binascii.hexlify(RSA.generate(1024, Crypto.Random.new().read).exportKey(format='DER')).decode('ascii')
+
 
 def base16_to_base58(x):
     words = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     b58 = ''
     total = int(x, 16)
-    while total > 0:
+    while True:
         b58 += words[total%len(words)]
         if total < len(words):
-            b58 += words[len(words)]
+            b58 += words[total]
             break
         total //= len(words)
     return b58
 
-print(base16_to_base58('acd123'))
+print(base16_to_base58(public_key))
